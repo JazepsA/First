d5dbbed281484fc8be9a30daf60fa095ed695043
JazepsA <jazepsaugustans26@gmail.com>
2024-09-10 12:07:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello!")
+#print("hello!")
+
+Saraksts=["Anna", "Bruno", "Centis", "Dace"]
+a=str(input("Ievadiet vardu ,ko pielikt sarakstam: "))
+print(a)
+Saraksts.append(a)
+print(Saraksts)
+Saraksts.remove("Dace")
+print(f'Saraksts bez atteikusas personas {Saraksts}')
